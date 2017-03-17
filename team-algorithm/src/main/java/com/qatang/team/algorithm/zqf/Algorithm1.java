ef2004bc4bf92ab1a4bcaa5292ed0f00789da251
tangpengfei <tangpengfei@8win.com>
2017-03-17 15:34:02 +0800

    first commit

---
+++ team-algorithm/src/main/java/com/qatang/team/algorithm/zqf/Algorithm1.java
@@ -0,0 +1,11 @@
+package com.qatang.team.algorithm.zqf;
+
+/**
+ * @author qatang
+ * @since 2017-03-17 15:33
+ */
+public class Algorithm1 {
+    public static void main(String[] args) {
+
+    }
+}
