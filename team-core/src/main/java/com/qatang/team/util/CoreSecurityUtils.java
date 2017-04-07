ec980a171635d6ef71cdbd57c1bb2a5050abf99f
tangpengfei <tangpengfei@8win.com>
2017-04-07 17:19:48 +0800

    添加CoreSecurityUtils类及测试用例

---
+++ team-core/src/main/java/com/qatang/team/util/CoreSecurityUtils.java
@@ -0,0 +1,83 @@
+package com.qatang.team.util;
+
+import com.google.common.hash.Hashing;
+import com.google.common.io.BaseEncoding;
+
+/**
+ * 安全工具集合类
+ *
+ * 转码工具: 编码 解码
+ *  BASE64、HEX
+ *
+ * 消息摘要：单向散列
+ *  MD5、SHA1、CRC32
+ *
+ * @author qatang
+ */
+public class CoreSecurityUtils {
+
+    /**
+     * base64编码
+     * @param input 输入数据
+     * @return 编码结果
+     */
+    public static String encodeBase64(byte[] input) {
+        return BaseEncoding.base64().encode(input);
+    }
+
+    /**
+     * base64解码
+     * @param encodeString 已编码字符串
+     * @return 解码结果
+     */
+    public static byte[] decodeBase64(String encodeString) {
+        return BaseEncoding.base64().decode(encodeString);
+    }
+
+    /**
+     * 十六进制编码
+     * @param input 输入数据
+     * @return 编码结果
+     */
+    public static String encodeHex(byte[] input) {
+        return BaseEncoding.base16().encode(input);
+    }
+
+    /**
+     * 十六进制解码
+     * @param encodeString 已编码字符串
+     * @return 解码结果
+     */
+    public static byte[] decodeHex(String encodeString) {
+        return BaseEncoding.base16().decode(encodeString);
+    }
+
+    /**
+     * MD5摘要
+     * @param input 输入数据
+     * @return 摘要结果
+     */
+    public static String digestMD5(byte[] input) {
+        return Hashing.md5().hashBytes(input).toString();
+    }
+
+    /**
+     * SHA1摘要
+     * @param input 输入数据
+     * @return 摘要结果
+     */
+    public static String digestSHA1(byte[] input) {
+        return Hashing.sha1().hashBytes(input).toString();
+    }
+
+    /**
+     * CRC32摘要
+     * 结果与jdk自带crc32结果不一致，该方法只用于内部业务逻辑计算checksum值，不能对外业务使用
+     * 参照 <a href="https://github.com/google/guava/issues/1332">https://github.com/google/guava/issues/1332</>
+     * @param input 输入数据
+     * @return 摘要结果
+     */
+    public static long digestCRC32(byte[] input) {
+        return Hashing.crc32c().hashBytes(input).padToLong();
+    }
+}
---
+++ team-core/src/test/java/com/qatang/team/util/CoreSecurityUtilsTest.java
@@ -0,0 +1,89 @@
+package com.qatang.team.util;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * 安全工具集合测试类
+ *
+ * @author qatang
+ */
+public class CoreSecurityUtilsTest {
+    /**
+     * base64编码测试
+     */
+    @Test
+    public void encodeBase64() {
+        String inputString = "测试abc123";
+        String rightResult = "5rWL6K+VYWJjMTIz";
+        String result = CoreSecurityUtils.encodeBase64(inputString.getBytes());
+        Assert.assertTrue(rightResult.equals(result));
+    }
+
+    /**
+     * base64解码测试
+     */
+    @Test
+    public void decodeBase64() {
+        String inputString = "5rWL6K+VYWJjMTIz";
+        String rightResult = "测试abc123";
+        byte[] result = CoreSecurityUtils.decodeBase64(inputString);
+        Assert.assertTrue(rightResult.equals(new String(result)));
+    }
+
+    /**
+     * 十六进制编码测试
+     */
+    @Test
+    public void encodeHex() {
+        String inputString = "测试abc123";
+        String rightResult = "E6B58BE8AF95616263313233";
+        String result = CoreSecurityUtils.encodeHex(inputString.getBytes());
+        Assert.assertTrue(rightResult.equals(result));
+    }
+
+    /**
+     * 十六进制解码测试
+     */
+    @Test
+    public void decodeHex() {
+        String inputString = "E6B58BE8AF95616263313233";
+        String rightResult = "测试abc123";
+        byte[] result = CoreSecurityUtils.decodeHex(inputString);
+        Assert.assertTrue(rightResult.equals(new String(result)));
+    }
+
+    /**
+     * MD5摘要
+     */
+    @Test
+    public void digestMD5() {
+        String inputString = "测试abc123";
+        String rightResult = "7922c1b52d7047c55139da5ece7c4096";
+        String result = CoreSecurityUtils.digestMD5(inputString.getBytes());
+        Assert.assertTrue(rightResult.equals(result));
+    }
+
+    /**
+     * SHA1摘要测试
+     */
+    @Test
+    public void digestSHA1() {
+        String inputString = "测试abc123";
+        String rightResult = "5618437047ae57e26a0b042e54c156c0396d89ac";
+        String result = CoreSecurityUtils.digestSHA1(inputString.getBytes());
+        Assert.assertTrue(rightResult.equals(result));
+    }
+
+    /**
+     * CRC32摘要测试
+     */
+    @Test
+    public void digestCRC32() {
+        String inputString = "测试abc123";
+        long rightResult = 3782298985L;
+        long result = CoreSecurityUtils.digestCRC32(inputString.getBytes());
+        System.out.println(result);
+        Assert.assertTrue(rightResult == result);
+    }
+}
