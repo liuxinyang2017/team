934906925e91c69b63801ad8285aee3fd76a568a
tangpengfei <tangpengfei@8win.com>
2017-04-21 11:34:25 +0800

    增加DESede、AES加密算法

--- team-core/team-core-base/src/main/java/com/qatang/team/util/CoreSecurityUtils.java
+++ team-core/team-core-base/src/main/java/com/qatang/team/util/CoreSecurityUtils.java
@@ -4,6 +4,11 @@ import com.google.common.hash.Hashing;
 import com.google.common.io.BaseEncoding;
 
 import javax.crypto.Cipher;
+import javax.crypto.SecretKey;
+import javax.crypto.SecretKeyFactory;
+import javax.crypto.spec.DESedeKeySpec;
+import javax.crypto.spec.IvParameterSpec;
+import javax.crypto.spec.SecretKeySpec;
 import java.security.KeyFactory;
 import java.security.PrivateKey;
 import java.security.PublicKey;
@@ -183,6 +188,201 @@ public class CoreSecurityUtils {
             //解密
             Cipher cipher = Cipher.getInstance("RSA/ECB/PKCS1Padding");// 不能直接使用RSA，要写全转换模式，不然与android不一致
             cipher.init(Cipher.DECRYPT_MODE, privateKey);
+            return cipher.doFinal(input);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    /**
+     * 3des加密
+     * ECB模式，不需要初始化向量
+     * @param input 源数据
+     * @param encodedKey 私钥
+     * @return 加密后的结果
+     */
+    public static byte[] encryptDESede(byte[] input, byte[] encodedKey) throws RuntimeException {
+        try {
+            //构造DESedeKeySpec对象
+            DESedeKeySpec deSedeKeySpec = new DESedeKeySpec(encodedKey);
+            //指定加密算法
+            SecretKeyFactory secretKeyFactory = SecretKeyFactory.getInstance("DESede");
+            //取公钥匙对象
+            SecretKey secretKey = secretKeyFactory.generateSecret(deSedeKeySpec);
+            //加密
+            Cipher cipher = Cipher.getInstance("DESede/ECB/PKCS5Padding");
+            cipher.init(Cipher.ENCRYPT_MODE, secretKey);
+
+            return cipher.doFinal(input);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    /**
+     * 3des解密
+     * ECB模式，不需要初始化向量
+     * @param input 源数据
+     * @param encodedKey 私钥
+     * @return 解密后的结果
+     */
+    public static byte[] decryptDESede(byte[] input, byte[] encodedKey) throws RuntimeException {
+        try {
+            //构造DESedeKeySpec对象
+            DESedeKeySpec deSedeKeySpec = new DESedeKeySpec(encodedKey);
+            //指定加密算法
+            SecretKeyFactory secretKeyFactory = SecretKeyFactory.getInstance("DESede");
+            //取公钥匙对象
+            SecretKey secretKey = secretKeyFactory.generateSecret(deSedeKeySpec);
+            //解密
+            Cipher cipher = Cipher.getInstance("DESede/ECB/PKCS5Padding");
+            cipher.init(Cipher.DECRYPT_MODE, secretKey);
+
+            return cipher.doFinal(input);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    /**
+     * 3des加密
+     * CBC模式，需要64位初始化向量
+     * @param input 源数据
+     * @param encodedKey 私钥
+     * @param iv 8字节初始化向量
+     * @return 加密后的结果
+     */
+    public static byte[] encryptDESede(byte[] input, byte[] encodedKey, byte[] iv) throws RuntimeException {
+        try {
+            //构造DESedeKeySpec对象
+            DESedeKeySpec deSedeKeySpec = new DESedeKeySpec(encodedKey);
+            //指定加密算法
+            SecretKeyFactory secretKeyFactory = SecretKeyFactory.getInstance("DESede");
+            //取公钥匙对象
+            SecretKey secretKey = secretKeyFactory.generateSecret(deSedeKeySpec);
+            // 初始化向量
+            IvParameterSpec ivParameterSpec = new IvParameterSpec(iv);
+            //加密
+            Cipher cipher = Cipher.getInstance("DESede/CBC/PKCS5Padding");
+            cipher.init(Cipher.ENCRYPT_MODE, secretKey, ivParameterSpec);
+
+            return cipher.doFinal(input);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    /**
+     * 3des解密
+     * CBC模式，需要64位初始化向量
+     * @param input 源数据
+     * @param encodedKey 私钥
+     * @param iv 8字节初始化向量，与加密时保持一致
+     * @return 解密后的结果
+     */
+    public static byte[] decryptDESede(byte[] input, byte[] encodedKey, byte[] iv) throws RuntimeException {
+        try {
+            //构造DESedeKeySpec对象
+            DESedeKeySpec deSedeKeySpec = new DESedeKeySpec(encodedKey);
+            //指定加密算法
+            SecretKeyFactory secretKeyFactory = SecretKeyFactory.getInstance("DESede");
+            //取公钥匙对象
+            SecretKey secretKey = secretKeyFactory.generateSecret(deSedeKeySpec);
+            // 初始化向量
+            IvParameterSpec ivParameterSpec = new IvParameterSpec(iv);
+            //解密
+            Cipher cipher = Cipher.getInstance("DESede/CBC/PKCS5Padding");
+            cipher.init(Cipher.DECRYPT_MODE, secretKey, ivParameterSpec);
+            return cipher.doFinal(input);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    /**
+     * aes加密
+     * ECB模式，不需要初始化向量
+     * @param input 源数据
+     * @param encodedKey 私钥
+     * @return 加密后的结果
+     */
+    public static byte[] encryptAES(byte[] input, byte[] encodedKey) throws RuntimeException {
+        try {
+            //构造SecretKeySpec对象
+            SecretKeySpec secretKeySpec = new SecretKeySpec(encodedKey, "AES");
+
+            //加密
+            Cipher cipher = Cipher.getInstance("AES/ECB/PKCS5Padding");
+            cipher.init(Cipher.ENCRYPT_MODE, secretKeySpec);
+
+            return cipher.doFinal(input);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    /**
+     * aes解密
+     * ECB模式，不需要初始化向量
+     * @param input 源数据
+     * @param encodedKey 私钥
+     * @return 解密后的结果
+     */
+    public static byte[] decryptAES(byte[] input, byte[] encodedKey) throws RuntimeException {
+        try {
+            //构造SecretKeySpec对象
+            SecretKeySpec secretKeySpec = new SecretKeySpec(encodedKey, "AES");
+            //解密
+            Cipher cipher = Cipher.getInstance("AES/ECB/PKCS5Padding");
+            cipher.init(Cipher.DECRYPT_MODE, secretKeySpec);
+
+            return cipher.doFinal(input);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    /**
+     * aes加密
+     * CBC模式，需要128位初始化向量
+     * @param input 源数据
+     * @param encodedKey 私钥
+     * @param iv 16字节初始化向量
+     * @return 加密后的结果
+     */
+    public static byte[] encryptAES(byte[] input, byte[] encodedKey, byte[] iv) throws RuntimeException {
+        try {
+            //构造SecretKeySpec对象
+            SecretKeySpec secretKeySpec = new SecretKeySpec(encodedKey, "AES");
+            // 初始化向量
+            IvParameterSpec ivParameterSpec = new IvParameterSpec(iv);
+            //加密
+            Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
+            cipher.init(Cipher.ENCRYPT_MODE, secretKeySpec, ivParameterSpec);
+
+            return cipher.doFinal(input);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    /**
+     * AES解密
+     * CBC模式，需要128位初始化向量
+     * @param input 源数据
+     * @param encodedKey 私钥
+     * @param iv 16字节初始化向量，与加密时保持一致
+     * @return 解密后的结果
+     */
+    public static byte[] decryptAES(byte[] input, byte[] encodedKey, byte[] iv) throws RuntimeException {
+        try {
+            //构造SecretKeySpec对象
+            SecretKeySpec secretKeySpec = new SecretKeySpec(encodedKey, "AES");
+            // 初始化向量
+            IvParameterSpec ivParameterSpec = new IvParameterSpec(iv);
+            //解密
+            Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
+            cipher.init(Cipher.DECRYPT_MODE, secretKeySpec, ivParameterSpec);
             return cipher.doFinal(input);
         } catch (Exception e) {
             throw new RuntimeException(e);
