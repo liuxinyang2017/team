515384341f7189cb100197d7d3549b9c90c729ba
tangpengfei <tangpengfei@8win.com>
2017-07-19 17:31:36 +0800

    代理抓取及测试定时处理器

--- team-fetcher/src/main/java/com/qatang/team/fetcher/worker/AbstractNumberLotteryFetcher.java
+++ team-fetcher/src/main/java/com/qatang/team/fetcher/worker/AbstractNumberLotteryFetcher.java
@@ -1,17 +1,21 @@
 package com.qatang.team.fetcher.worker;
 
 import com.google.common.base.Strings;
+import com.google.common.collect.Maps;
+import com.qatang.team.core.util.CoreHttpUtils;
 import com.qatang.team.enums.fetcher.FetcherType;
 import com.qatang.team.enums.lottery.LotteryType;
 import com.qatang.team.fetcher.bean.NumberLotteryFetchResult;
 import com.qatang.team.fetcher.exception.FetcherException;
-import com.qatang.team.proxy.ProxyManager;
+import com.qatang.team.proxy.bean.ProxyInfo;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.net.ProxySelector;
+import java.net.InetSocketAddress;
+import java.net.Proxy;
+import java.util.Map;
 
 /**
  * @author qatang
@@ -44,12 +48,18 @@ public abstract class AbstractNumberLotteryFetcher implements INumberLotteryFetc
     protected abstract String getDetailFetchUrl();
 
     /**
+     * 获取抓取网站字符编码
+     * @return 抓取网站字符编码
+     */
+    protected abstract String getFetchEncoding();
+
+    /**
      * 解析开奖号码抓取内容
      * @param phase 彩期
      * @param document 抓取内容
      * @return 抓取数据
      */
-    protected abstract NumberLotteryFetchResult parseResultDoc(String phase, Document document);
+    protected abstract NumberLotteryFetchResult parseResultDoc(String phase, Document document, ProxyInfo proxyInfo);
 
     /**
      * 解析开奖详情抓取内容
@@ -57,7 +67,7 @@ public abstract class AbstractNumberLotteryFetcher implements INumberLotteryFetc
      * @param document 抓取内容
      * @return 抓取数据
      */
-    protected abstract NumberLotteryFetchResult parseDetailDoc(String phase, Document document);
+    protected abstract NumberLotteryFetchResult parseDetailDoc(String phase, Document document, ProxyInfo proxyInfo);
 
     /**
      * 抓取源数据
@@ -65,12 +75,51 @@ public abstract class AbstractNumberLotteryFetcher implements INumberLotteryFetc
      * @return 源数据
      * @throws FetcherException 异常
      */
-    protected Document fetch(String url) throws FetcherException {
+    protected Document fetch(String url, String encoding, ProxyInfo proxyInfo) throws FetcherException {
         try {
-            return Jsoup.connect(url)
-                    .userAgent("Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36")
-                    .timeout(10000)
-                    .get();
+            Map<String, String> headers = Maps.newHashMap();
+            headers.put("User-Agent", "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36");
+
+            Proxy proxy = null;
+            String username = null;
+            String password = null;
+            if (proxyInfo != null) {
+                proxy = new Proxy(Proxy.Type.HTTP, new InetSocketAddress(proxyInfo.getHost(), proxyInfo.getPort()));
+                username = proxyInfo.getUsername();
+                password = proxyInfo.getPassword();
+            }
+            String html = CoreHttpUtils.getWithHeadersByProxy(url, headers, encoding, 10, proxy, username, password);
+            return Jsoup.parse(html);
+        } catch (Exception e) {
+            logger.error(e.getMessage(), e);
+            throw new FetcherException(e.getMessage());
+        }
+    }
+
+    @Override
+    public NumberLotteryFetchResult fetchResult(String phase, ProxyInfo proxyInfo) throws FetcherException {
+        if (Strings.isNullOrEmpty(phase)) {
+            throw new FetcherException("彩期不能为空");
+        }
+        logger.info(String.format("开始抓开奖号码，lotteryType=%s，fetcherType=%s, phase=%s", this.getLotteryType().getName(), this.getFetcherType().getName(), phase));
+        Document doc = this.fetch(this.getResultFetchUrl(), this.getFetchEncoding(), proxyInfo);
+        try {
+            return parseResultDoc(phase, doc, proxyInfo);
+        } catch (Exception e) {
+            logger.error(e.getMessage(), e);
+            throw new FetcherException(e.getMessage());
+        }
+    }
+
+    @Override
+    public NumberLotteryFetchResult fetchDetail(String phase, ProxyInfo proxyInfo) throws FetcherException {
+        if (Strings.isNullOrEmpty(phase)) {
+            throw new FetcherException("彩期不能为空");
+        }
+        logger.info(String.format("开始抓开奖详情，lotteryType=%s，fetcherType=%s, phase=%s", this.getLotteryType().getName(), this.getFetcherType().getName(), phase));
+        Document doc = this.fetch(this.getDetailFetchUrl(), this.getFetchEncoding(), proxyInfo);
+        try {
+            return parseDetailDoc(phase, doc, proxyInfo);
         } catch (Exception e) {
             logger.error(e.getMessage(), e);
             throw new FetcherException(e.getMessage());
@@ -79,31 +128,11 @@ public abstract class AbstractNumberLotteryFetcher implements INumberLotteryFetc
 
     @Override
     public NumberLotteryFetchResult fetchResult(String phase) throws FetcherException {
-        if (Strings.isNullOrEmpty(phase)) {
-            throw new FetcherException("彩期不能为空");
-        }
-        logger.info(String.format("开始抓开奖号码，lotteryType=%s，fetcherType=%s, phase=%s", this.getLotteryType().getName(), this.getFetcherType().getName(), phase));
-        Document doc = this.fetch(this.getResultFetchUrl());
-        try {
-            return parseResultDoc(phase, doc);
-        } catch (Exception e) {
-            logger.error(e.getMessage(), e);
-            throw new FetcherException(e.getMessage());
-        }
+        return this.fetchResult(phase, null);
     }
 
     @Override
     public NumberLotteryFetchResult fetchDetail(String phase) throws FetcherException {
-        if (Strings.isNullOrEmpty(phase)) {
-            throw new FetcherException("彩期不能为空");
-        }
-        logger.info(String.format("开始抓开奖详情，lotteryType=%s，fetcherType=%s, phase=%s", this.getLotteryType().getName(), this.getFetcherType().getName(), phase));
-        Document doc = this.fetch(this.getDetailFetchUrl());
-        try {
-            return parseDetailDoc(phase, doc);
-        } catch (Exception e) {
-            logger.error(e.getMessage(), e);
-            throw new FetcherException(e.getMessage());
-        }
+        return this.fetchDetail(phase, null);
     }
 }
--- team-fetcher/src/main/java/com/qatang/team/fetcher/worker/INumberLotteryFetcher.java
+++ team-fetcher/src/main/java/com/qatang/team/fetcher/worker/INumberLotteryFetcher.java
@@ -2,6 +2,7 @@ package com.qatang.team.fetcher.worker;
 
 import com.qatang.team.fetcher.bean.NumberLotteryFetchResult;
 import com.qatang.team.fetcher.exception.FetcherException;
+import com.qatang.team.proxy.bean.ProxyInfo;
 
 /**
  * @author qatang
@@ -22,4 +23,22 @@ public interface INumberLotteryFetcher {
      * @throws FetcherException 异常
      */
     NumberLotteryFetchResult fetchDetail(String phase) throws FetcherException;
+
+    /**
+     * 抓取开奖号码
+     * @param phase 彩期
+     * @param proxyInfo 代理信息
+     * @return 开奖号码
+     * @throws FetcherException 异常
+     */
+    NumberLotteryFetchResult fetchResult(String phase, ProxyInfo proxyInfo) throws FetcherException;
+
+    /**
+     * 抓取开奖详情
+     * @param phase 彩期
+     * @param proxyInfo 代理信息
+     * @return 开奖详情
+     * @throws FetcherException 异常
+     */
+    NumberLotteryFetchResult fetchDetail(String phase, ProxyInfo proxyInfo) throws FetcherException;
 }
