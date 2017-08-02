703e9534d754f3afe370cc8e3d5e492251ebf373
jinsheng <sheng.jin@8win.com>
2017-08-02 17:14:30 +0800

    删除无用类引用

--- team-api/team-fetcher-service/src/test/java/com/qatang/team/fetcher/config/InitConfig.java
+++ team-api/team-fetcher-service/src/test/java/com/qatang/team/fetcher/config/InitConfig.java
@@ -1,13 +1,6 @@
 package com.qatang.team.fetcher.config;
 
-import com.fasterxml.jackson.annotation.JsonInclude;
-import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.SerializationFeature;
-import com.fasterxml.jackson.databind.module.SimpleModule;
-import com.fasterxml.jackson.datatype.jdk8.Jdk8Module;
-import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
-import com.fasterxml.jackson.module.paramnames.ParameterNamesModule;
 import com.qatang.team.json.CustomObjectMapper;
 import org.springframework.context.annotation.*;
 import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
